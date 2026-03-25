8ab8f391a7177f7b1efb3c39094581aa33163a5e
Nicolas Raffonaud <nicolasraffonaudsoft@gmail.com>
2026-03-25 18:56:29 -0300

    feat(frontend): integrar react-query como capa de data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,23 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { initWebVitals } from "./utils/webVitals";
+import { queryClient } from "./lib/react-query";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+      {import.meta.env.DEV ? <ReactQueryDevtools initialIsOpen={false} /> : null}
+    </QueryClientProvider>
   </React.StrictMode>,
 );
 
